1a7359fed43a73c6ffb4127b16d56bc70d966701
agentquay <marquay.smith@gmail.com>
2014-07-17 01:17:37 -0400

    slider

--- js/main.js
+++ js/main.js
@@ -13,3 +13,48 @@ $(window).bind('scroll', function () {
     }
 });
 
+
+
+
+//background image rotator
+var imgArr = new Array('https://raw.githubusercontent.com/agentquay/jfagan/master/img/trees.jpg', 'https://raw.githubusercontent.com/agentquay/jfagan/master/img/cliffs.jpg', 'https://raw.githubusercontent.com/agentquay/jfagan/master/img/clovers.jpg');
+//var imgArr = new Array('trees', 'cliffs', 'clovers');
+
+var preloadArr = new Array();
+var i;
+
+/* preload images */
+for (i = 0; i < imgArr.length; i++) {
+    preloadArr[i] = new Image();
+    preloadArr[i].src = imgArr[i];
+}
+
+var currImg = 1;
+var intID = setInterval(changeImg, 1000);
+
+//function changeImg() {
+  //  $('.head').css({ 'background': 'url(' +  preloadArr[currImg++ % preloadArr.length].src + ')' });
+//}
+
+//function changeImg() {
+//	$( ".head" ).fadeIn( "slow", function() {
+  //  $('.head').toggleClass( preloadArr[currImg++ % preloadArr.length]);
+
+    // });
+//}
+
+function changeImg() {
+    $('.head').css({ 
+
+    	'background': 'url(' +  preloadArr[currImg++ % preloadArr.length].src + ')' 
+
+    }).fadeIn("slow");
+}
+
+
+
+
+
+
+
+
